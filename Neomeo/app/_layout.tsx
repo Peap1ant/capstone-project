03644220fa6b13c3b4120858e127e177b917d001
Peaplant <peaplant4@gmail.com>
2025-09-26 19:46:32 +0900

    Update

--- Neomeo/app/_layout.tsx
+++ Neomeo/app/_layout.tsx
@@ -1,12 +1,9 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {/* 로그인/회원가입 */}
-      <Stack.Screen name="(bottom)" options={{ headerShown: false }} />
-      {/* 탭 네비게이션 */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+        </Stack>
+    );
 }
