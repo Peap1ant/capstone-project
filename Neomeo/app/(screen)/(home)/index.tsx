1650f36422e2a6aeafd4ca3a31b1bd0d8ff33a69
= <peaplant4@gmail.com>
2025-09-29 11:29:04 +0900

    update

--- Neomeo/app/(screen)/(home)/index.tsx
+++ Neomeo/app/(screen)/(home)/index.tsx
@@ -1,13 +1,12 @@
 import { View, Text } from 'react-native';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import {styles} from '../../../styles/test_style.js';
 
 export function HomeScreen() {
-    const insets = useSafeAreaInsets();
     return (
-        <View style = {styles.container}>
+        <SafeAreaView style = {[styles.container, styles.content_center]}>
             <Text style = {styles.text}> Tab Home</Text>
-        </View>
+        </SafeAreaView>
     );
 }
 
